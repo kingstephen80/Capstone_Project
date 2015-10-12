297bfab177b32eb21de4a6e3ec00791836fb74cd
kingstephen80 <stephen.king.pdx@gmail.com>
2015-10-12 16:47:54 -0700

    updated the urls  & views to reflect the html pages I have created.
    ongoing issues
    1. css is removed from homepage between login and logout.
    2. links for editing and new posts, are not working
    3. links for pill-nav-tabs are not set.
    
    10-12-15
    trying to work out what is removing the css from the login logout page

--- DIY_Electronics/electronix_pdx/urls.py
+++ DIY_Electronics/electronix_pdx/urls.py
@@ -5,7 +5,7 @@ from .import views
 urlpatterns = [
     url(r'^$', views.new_blog_post, name='new_blog_post'),
     url(r'^all/$', views.blogposts_all, name='blogposts_all'),
-    url(r'^blogpost/(?P<pk>[0-9]+)/$', views.blogpost_single, name='blogpost_single'),
+    url(r'^single/(?P<pk>[0-9]+)/$', views.blogpost_single, name='blogpost_single'),
     url(r'^edit/(?P<pk>[0-9]+)/blogpost_single/$', views.edit_blogpost, name='edit_blogpost'),
     url(r'^draft/$', views.blogpost_draft, name='blogpost_draft'),
 
